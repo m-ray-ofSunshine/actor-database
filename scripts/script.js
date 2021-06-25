8079ff6efb10366cba56f2fcd31d2714a5f0024b
Hewan Redie <tewol22@gmail.com>
2021-06-25 13:53:48 -0500

    updated bio and image functions

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,15 @@ $("#searchBtn").on("click", storeInput)
 var searchInput = localStorage.getItem("search")
 var apiKey = "65e03376af118d009632cee16530207e"
 var apiUrlPersonSearch = "https://api.themoviedb.org/3/search/person?api_key="+ apiKey + "&query=" + searchInput
-var personID; 
+
 var apiUrlPersonGetDetails;
 var apiUrlPersonGetMovieCredits;
 var apiUrlPersonGetTwitterID;
 var personTwitterID;
+
 var apiUrlgetPersonImage;
+var apiUrlgetPersonBio;
+var apiUrlgetPersonName;
 
 function getPersonID() {
     fetch(apiUrlPersonSearch)
@@ -15,44 +18,43 @@ function getPersonID() {
         return response.json()
     })
     .then(function (data) {
-        personID = data.results[0].id;
-        apiUrlPersonGetDetails = "https://api.themoviedb.org/3/person/" + personID + "?api_key=" + apiKey 
-        apiUrlPersonGetMovieCredits = "https://api.themoviedb.org/3/person/" + personID+ "/movie_credits?api_key="+ apiKey
-        apiUrlPersonGetTwitterID = "https://api.themoviedb.org/3/person/" + personID + "/external_ids?api_key=" + apiKey 
-        apiUrlgetPersonImage = "https://api.themoviedb.org/3/person/" +personID + "/images?api_key=" + apiKey;
-        })
-
+        personID = data.results[0].id; 
+    })
+    
 }
 function getPersonDetails() {
+    apiUrlPersonGetDetails = "https://api.themoviedb.org/3/person/" + personID + "?api_key=" + apiKey 
     fetch(apiUrlPersonGetDetails)
-        .then(function (response) {
+    .then(function (response) {
         return response.json()
-        })
-        .then(function (data) {
-            //console.log(data);
-
-        })
+    })
+    .then(function (data) {
+        //console.log(data);
+        
+    })
 }
 function getPersonMovieCredits() {
+    apiUrlPersonGetMovieCredits = "https://api.themoviedb.org/3/person/" + personID+ "/movie_credits?api_key="+ apiKey
     fetch(apiUrlPersonGetMovieCredits)
-        .then(function (response) {
+    .then(function (response) {
         return response.json()
-        })
-        .then(function (data) {
-            //console.log(data);
-
-        })
+    })
+    .then(function (data) {
+        //console.log(data);
+        
+    })
 }
 function getPersonTwitterID() {
+    apiUrlPersonGetTwitterID = "https://api.themoviedb.org/3/person/" + personID + "/external_ids?api_key=" + apiKey 
     fetch(apiUrlPersonGetTwitterID)
-        .then(function (response) {
+    .then(function (response) {
         return response.json()
-        })
-        .then(function (data) {
-            //console.log(data);
-            personTwitterID = data.twitter_id;
-
-        })
+    })
+    .then(function (data) {
+        //console.log(data);
+        personTwitterID = data.twitter_id;
+        
+    })
 }
 
 function storeInput() {
@@ -62,14 +64,35 @@ function storeInput() {
 }
 
 function getPersonImage() {
+    apiUrlgetPersonImage = "https://api.themoviedb.org/3/person/" +personID + "/images?api_key=" + apiKey;
     fetch(apiUrlgetPersonImage)
-        .then(function (response) {
+    .then(function (response) {
         return response.json()
-        })
-        .then(function (data) {
-            console.log(data);
-            console.log("https://www.themoviedb.org/t/p/original" + data.profiles[0].file_path );
-            $("#personImage").attr("src","https://www.themoviedb.org/t/p/original" + data.profiles[0].file_path  );
-
-        })
+    })
+    .then(function (data) {
+        console.log(data);
+        console.log("https://www.themoviedb.org/t/p/original" + data.profiles[0].file_path );
+        
+        $("#personImage").attr("src","https://www.themoviedb.org/t/p/original" + data.profiles[0].file_path );
+        $("#personImage").attr({width: "200", height: "250"} );
+        
+    })
 }
+
+function getPersonBio() {
+    apiUrlgetPersonBio = "https://api.themoviedb.org/3/person/" + personID + "?api_key=" + apiKey;
+    fetch(apiUrlgetPersonBio)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        //console.log(data);
+        console.log(data.biography);
+        //alert("Text: " 
+        $("#personBio").text(data.biography);
+        $("#personName").text(data.name);
+        
+    })
+}
+
+
